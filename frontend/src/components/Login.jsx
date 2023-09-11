3fa36e0f4d95062ebb08c97651406b75b2306b6f
thalha-dev <dev.thalha@gmail.com>
2023-09-11 21:12:09 +0530

    implement signup and login page, add styles

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,7 +1,84 @@
+import { useEffect, useState } from "react";
+import { AiFillWarning } from "react-icons/ai";
+import {
+  getIndividualErrorMessageCB,
+  getIndividualErrorMessageFromCB,
+  getLoginStatusCB,
+  login,
+  refreshAccessTokenInitial,
+} from "../../state/slice/userSlice";
+import { useDispatch, useSelector } from "react-redux";
+import { Navigate } from "react-router-dom";
+
 const Login = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const loginStatus = useSelector(getLoginStatusCB);
+  const errorMessage = useSelector(getIndividualErrorMessageCB);
+  const errorMessageFrom = useSelector(getIndividualErrorMessageFromCB);
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (loginStatus !== "success") {
+      dispatch(refreshAccessTokenInitial());
+    }
+  }, []);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const userCredentials = {
+      username: username,
+      password: password,
+    };
+
+    dispatch(login(userCredentials));
+  };
+
   return (
-    <div>
-      <h2>Login</h2>
+    <div className="login-form-container" onSubmit={handleSubmit}>
+      {loginStatus === "success" ? <Navigate to="/home" /> : ""}
+      <form>
+        <h1>LOGIN</h1>
+        <input
+          type="text"
+          placeholder="Username"
+          value={username}
+          onChange={(e) => {
+            setUsername(e.target.value);
+          }}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => {
+            setPassword(e.target.value);
+          }}
+        />
+        <button
+          className={`login-button ${
+            loginStatus === "loading" ? "button-disable" : ""
+          }`}
+        >
+          SUBMIT
+        </button>
+        {errorMessage && errorMessageFrom === "login" ? (
+          <div className="login-error-msg">
+            <AiFillWarning className="login-error-sign" />
+            <div>{errorMessage}</div>
+          </div>
+        ) : (
+          ""
+        )}
+        {loginStatus === "loading" ? (
+          <div className="login-loading-msg">loading...</div>
+        ) : (
+          ""
+        )}
+      </form>
     </div>
   );
 };
--- frontend/src/components/Signup.jsx
+++ frontend/src/components/Signup.jsx
@@ -1,7 +1,93 @@
+import { useState } from "react";
+import { AiFillWarning } from "react-icons/ai";
+import {
+  getIndividualErrorMessageCB,
+  getIndividualErrorMessageFromCB,
+  getSignupStatusCB,
+  getLoginStatusCB,
+  signup,
+} from "../../state/slice/userSlice";
+import { useDispatch, useSelector } from "react-redux";
+import { Navigate } from "react-router-dom";
+
 const Signup = () => {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const signupStatus = useSelector(getSignupStatusCB);
+  const loginStatus = useSelector(getLoginStatusCB);
+  const errorMessage = useSelector(getIndividualErrorMessageCB);
+  const errorMessageFrom = useSelector(getIndividualErrorMessageFromCB);
+
+  const dispatch = useDispatch();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const userCredentials = {
+      username: username,
+      email: email,
+      password: password,
+      role: import.meta.env.VITE_ROLE_USER,
+    };
+
+    dispatch(signup(userCredentials));
+  };
+
   return (
-    <div>
-      <h2>signup</h2>
+    <div className="singup-form-container" onSubmit={handleSubmit}>
+      {signupStatus === "success" || loginStatus === "success" ? (
+        <Navigate to="/login" />
+      ) : (
+        <form>
+          <h1>SIGNUP</h1>
+          <input
+            type="text"
+            placeholder="Username"
+            value={username}
+            onChange={(e) => {
+              setUsername(e.target.value);
+            }}
+          />
+          <input
+            type="email"
+            placeholder="Email"
+            value={email}
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+          />
+          <button
+            className={`signup-button ${
+              signupStatus === "loading" ? "button-disable" : ""
+            }`}
+          >
+            SUBMIT
+          </button>
+          {errorMessage && errorMessageFrom === "signup" ? (
+            <div className="signup-error-msg">
+              <AiFillWarning className="signup-error-sign" />
+              <div>{errorMessage}</div>
+            </div>
+          ) : (
+            ""
+          )}
+          {signupStatus === "loading" ? (
+            <div className="signup-loading-msg">loading...</div>
+          ) : (
+            ""
+          )}
+        </form>
+      )}
     </div>
   );
 };
