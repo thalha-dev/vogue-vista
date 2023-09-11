4876884be23545026f453418a0f79974bdec74c3
thalha-dev <dev.thalha@gmail.com>
2023-09-11 13:44:46 +0530

    add login async thunk and add cases

--- frontend/state/slice/userSlice.js
+++ frontend/state/slice/userSlice.js
@@ -1,5 +1,6 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import api from "../axiosBase/api";
+import jwt_decode from "jwt-decode";
 
 const initialState = {
   token: null,
@@ -31,13 +32,28 @@ export const signup = createAsyncThunk(
   },
 );
 
+export const login = createAsyncThunk("user/login", async (userCredentials) => {
+  try {
+    const response = await api.post("/api/users/login", userCredentials, {
+      withCredentials: true,
+    });
+    return response.data;
+  } catch (error) {
+    const errorMessage = error.response?.data?.error;
+    if (errorMessage) {
+      throw new Error(errorMessage);
+    }
+    throw error;
+  }
+});
+
 const userSlice = createSlice({
   name: "user",
   initialState: initialState,
   reducers: {},
   extraReducers: (builder) => {
     builder
-      // singup
+      // -------------------------------------------------singup
       .addCase(signup.pending, (state) => {
         state.signupStatus = "loading";
         state.errorMessage = null;
@@ -50,8 +66,38 @@ const userSlice = createSlice({
         state.signupStatus = "failed";
         state.errorMessage = action.error.message;
         state.errorMessageFrom = "signup";
+      })
+      // -------------------------------------------------login
+      .addCase(login.pending, (state) => {
+        state.loginStatus = "loading";
+        state.errorMessage = null;
+      })
+      .addCase(login.fulfilled, (state, action) => {
+        const tokenRecieved = action.payload.accessToken;
+        const decoded = jwt_decode(tokenRecieved);
+        const rolesArray = decoded.UserInfo.roles || [];
+
+        rolesArray.forEach((roleGiven) => {
+          if (roleGiven === Number(import.meta.env.VITE_ROLE_ADMIN)) {
+            state.isAdmin = true;
+          }
+          if (roleGiven === Number(import.meta.env.VITE_ROLE_USER)) {
+            state.isUser = true;
+          }
+        });
+
+        state.token = tokenRecieved;
+        state.individualName = decoded?.UserInfo?.username || null;
+        state.individualId = decoded?.UserInfo?.userId || null;
+        state.loginStatus = "success";
+        state.refreshStatus = "success";
+        state.errorMessage = null;
+      })
+      .addCase(login.rejected, (state, action) => {
+        state.loginStatus = "failed";
+        state.errorMessage = action.error.message;
+        state.errorMessageFrom = "login";
       });
-    // ----------------------------------------------------
   },
 });
 
