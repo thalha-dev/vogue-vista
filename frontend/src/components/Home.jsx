71279dca9be7070241f8fb6949931ccb3d6d7c88
thalha-dev <dev.thalha@gmail.com>
2023-09-13 18:54:37 +0530

    implement search bar in home page

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,7 +1,27 @@
+import { BiSearch } from "react-icons/bi";
+import { useState } from "react";
+
 const Home = () => {
+  const [searchQuery, setSearchQuery] = useState("");
   return (
-    <div>
-      <h2>home</h2>
+    <div className="home-container">
+      <section className="home-search">
+        <div className="home-search-container">
+          <input
+            className="home-search-bar"
+            type="text"
+            name="searchIndividual"
+            value={searchQuery}
+            placeholder="Name, Brand, Color"
+            onChange={(e) => {
+              setSearchQuery(e.target.value);
+            }}
+          />
+          <button className="home-search-button">
+            <BiSearch />
+          </button>
+        </div>
+      </section>
     </div>
   );
 };
