861251b26c6cf31632d36ae4702f89315ae28295
thalha-dev <dev.thalha@gmail.com>
2023-09-20 19:47:38 +0530

    add redux-persist to persist user state in session-storage due to rehydration

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,24 +2,27 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { Provider } from "react-redux";
-import { store } from "../state/app/store.js";
+import { persistor, store } from "../state/app/store.js";
+import { PersistGate } from "redux-persist/integration/react";
 import { ToastContainer } from "react-toastify";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
-    <App />
-    <ToastContainer
-      position="top-right"
-      autoClose={8000}
-      hideProgressBar={false}
-      newestOnTop={false}
-      closeOnClick
-      rtl={false}
-      pauseOnFocusLoss
-      draggable
-      pauseOnHover
-      theme="light"
-    />
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+      <ToastContainer
+        position="top-right"
+        autoClose={8000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
+    </PersistGate>
   </Provider>,
 );
 {
--- frontend/state/app/store.js
+++ frontend/state/app/store.js
@@ -1,10 +1,23 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "../slice/userSlice";
 import shoeReducer from "../slice/shoeSlice";
+import storageSession from "redux-persist/lib/storage/session";
+import { persistReducer, persistStore } from "redux-persist";
+import thunk from "redux-thunk";
+
+const persistConfig = {
+  key: "user",
+  storage: storageSession,
+};
+
+const persistedReducer = persistReducer(persistConfig, userReducer);
 
 export const store = configureStore({
   reducer: {
-    user: userReducer,
+    user: persistedReducer,
     shoe: shoeReducer,
   },
+  middleware: [thunk],
 });
+
+export const persistor = persistStore(store);
