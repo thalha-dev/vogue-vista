304c7cde87ed29f4bb9985faea4c30eb99d9c649
thalha-dev <dev.thalha@gmail.com>
2023-09-23 15:24:51 +0530

    style landing page

--- frontend/src/components/LandingPage.jsx
+++ frontend/src/components/LandingPage.jsx
@@ -1,7 +1,26 @@
+import { Link } from "react-router-dom";
+import modelImage from "../assets/landing-page-model.png";
+
 const LandingPage = () => {
   return (
-    <div>
-      <h2>Landing Page</h2>
+    <div className="landing-page-container">
+      <section className="hero-section">
+        <h1>Fashion That Walks With You</h1>
+        <p>
+          Explore an exquisite collection of shoes curated for every style and
+          occasion.
+        </p>
+        <Link className="hero-shop-now-link" to="/home">
+          Shop Now
+        </Link>
+      </section>
+      <section className="landing-page-image-section">
+        <img
+          className="landing-page-model-image"
+          src={modelImage}
+          alt="landing page image"
+        />
+      </section>
     </div>
   );
 };
