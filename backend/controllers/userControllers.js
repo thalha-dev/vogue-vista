a34337c5260e69bf42d8a5855bea6ee4e47d7064
thalha-dev <dev.thalha@gmail.com>
2023-09-24 18:56:24 +0530

    add required options for setting/clearing jwt token in cookie

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -124,8 +124,8 @@ const login = async (req, res, next) => {
 
     res.cookie("jwt", refreshToken, {
       httpOnly: true,
-      // sameSite: "None",
-      // secure: true,
+      sameSite: "None",
+      secure: true,
       maxAge: 24 * 60 * 60 * 1000,
     });
 
@@ -150,8 +150,8 @@ const logout = async (req, res, next) => {
 
     res.clearCookie("jwt", {
       httpOnly: true,
-      // sameSite: "None",
-      // secure: true,
+      sameSite: "None",
+      secure: true,
     });
     res.sendStatus(204);
   } catch (error) {
